e80fbc5b861ce59155ca122134c0b1ed2149759b
Shinichi Okada <okada.shin@gmail.com>
2023-03-07 06:01:37 +0900

    fix: release.config.cjs by adding @ in front of semantic-release/git

--- release.config.cjs
+++ release.config.cjs
@@ -4,7 +4,7 @@ const config = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    ["semantic-release/git", {
+    ["@semantic-release/git", {
       "assets": ["dist/*.svelte", "dist/*.svelte.d.ts", "dist/*.js", "dist/*.d.ts"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],

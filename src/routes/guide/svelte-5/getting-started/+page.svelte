039a0baa35327ac7ee5e60c22080b98260c9825c
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-12-06 14:24:37 +0100

    fix: use svelte-shields

--- src/routes/guide/svelte-5/getting-started/+page.svelte
+++ src/routes/guide/svelte-5/getting-started/+page.svelte
@@ -1,5 +1,25 @@
 <script lang="ts">
   import { HighlightCompo, Code, H2 } from 'runes-webkit';
+  import { GitHubSponsor, type GitHubSponsorPropsType,  NpmVersion, type NpmVersionPropsType, License, type LicensePropsType, NpmDownload, type NpmDownloadPropsType } from 'svelte-shields';
+
+  const sponsor: GitHubSponsorPropsType = {
+    user: 'shinokada',
+    logo:'github',
+    color: 'red'
+  };
+  const npmversion: NpmVersionPropsType = {
+    packageName: 'svelte-ant-design-icons',
+    color: 'red',
+    logo:'svelte'
+  };
+  const license: LicensePropsType = {
+    source: 'github',
+    github_user: 'shinokada',
+    github_repo: 'svelte-ant-design-icons'
+  };
+  const downloads: NpmDownloadPropsType = {
+    packageName: 'svelte-ant-design-icons'
+  };
   const modules = import.meta.glob('./md/*.md', {
     query: '?raw',
     import: 'default',
@@ -10,36 +30,10 @@
 <h1>Getting Started - Svelte Ant Design Icons v2</h1>
 
 <div class="my-8 flex gap-2">
-  <a href="https://github.com/sponsors/shinokada" target="_blank" aria-label="Sponsor @shinokada"
-    ><img
-      src="https://img.shields.io/static/v1?label=Sponsor&message=%E2%9D%A4&logo=GitHub&color=%23fe8e86"
-      alt="sponsor"
-    /></a
-  >
-  <a
-    href="https://www.npmjs.com/package/svelte-ant-design-icons"
-    rel="nofollow"
-    target="_blank"
-    aria-label="npm svelte-ant-design-icons"
-    ><img src="https://img.shields.io/npm/v/svelte-ant-design-icons/next" alt="npm" /></a
-  >
-  <a
-    href="https://opensource.org/licenses/MIT"
-    rel="nofollow"
-    target="_blank"
-    aria-label="MIT License"
-    ><img
-      src="https://img.shields.io/github/license/shinokada/svelte-ant-design-icons"
-      alt="License"
-    /></a
-  >
-  <a
-    href="https://www.npmjs.com/package/svelte-ant-design-icons"
-    rel="nofollow"
-    target="_blank"
-    aria-label="npm svelte-ant-design-icons"
-    ><img src="https://img.shields.io/npm/dw/svelte-ant-design-icons.svg" alt="npm" /></a
-  >
+  <GitHubSponsor {...sponsor} />
+  <NpmVersion {...npmversion} />
+  <License {...license} />
+  <NpmDownload {...downloads} />
 </div>
 
 <H2>Requirements</H2>
